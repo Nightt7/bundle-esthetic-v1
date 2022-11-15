87509db7c3dd207079560ec07f6a1efbb9f30865
Night <101990128+Nightt7@users.noreply.github.com>
2022-11-15 13:26:14 +0100

    Add files via upload

---
+++ j-neonui/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.WhenUse = true
+
+Config.WhenKey = true
+
+Config.UseLogo  = true
+
+Config.LogoURL = ("https://cdn.discordapp.com/attachments/838123707980316724/1034480870486188042/New_Project_2.png")
